1dfe523491b8547533da14b6accde11a3dfd1043
Desai <kavish.desai@fiserv.com>
2023-12-14 16:20:58 -0500

    Uploading all the backend files

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="test_table")
+public class User {
+    @Id
+    @Column(name="id")
+    private int id;
+    private String name;
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+// Constructors, getters, setters, etc.
+}
---
+++ demo/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,66 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(int id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.orElse(null);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(int id, User userDetails) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            existingUser.setName(userDetails.getName());
+            existingUser.setGender(userDetails.getGender());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteUser(int id) {
+    userRepository.deleteById(id);
+    }
+    // This method below is an examples used to filter all person and return the one with gender "M"
+//    public List<TestTable> getAllTestTableRecords() {
+//        List<TestTable> allRecords = (List<TestTable>) testTableRepository.findAll();
+//
+//        List<TestTable> filteredRecords = new ArrayList<>();
+//        for (TestTable record : allRecords) {
+//            if ("M".equals(record.getGender())) {
+//                filteredRecords.add(record);
+//            }
+//        }
+//
+//        return filteredRecords;
+//
+//    }
+
+
+    // Other service methods for CRUD operations if needed
+}
